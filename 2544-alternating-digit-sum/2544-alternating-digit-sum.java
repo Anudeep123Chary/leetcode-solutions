f859cdda386880287aa6391f702f48ff1be9e8de
Anudeep123Chary <148636100+Anudeep123Chary@users.noreply.github.com>
2026-08-19 04:59:07 +0530

    Time: 0 ms (100%), Space: 42.3 MB (30.29%) - LeetHub

--- 2544-alternating-digit-sum/2544-alternating-digit-sum.java
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.java
@@ -1,12 +1,12 @@
 class Solution {
     public int alternateDigitSum(int n) {
-        int sum = 0;
+        int sum = 0; 
         int sign = 1;
-        while(n>0){
+        while(n>0) {
             sum += n%10 * sign;
             sign = -sign;
             n/=10;
         }
-        return -sign * sum;
+        return -sign*sum;
     }
 }
