5795b23bed709b5453879627e5d903156c567ea9
Anudeep123Chary <148636100+Anudeep123Chary@users.noreply.github.com>
2026-08-27 04:27:47 +0530

    Time: 13 ms (31.73%), Space: 46.6 MB (16.7%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -4,7 +4,7 @@ class Solution {
 
         int left = 0;
         int right = str.length()-1;
-        for(int i =0; i<s.length() && left<right; i++) {
+        while(left<right) {
             if(str.charAt(left++) != str.charAt(right--)) {
                 return false;
             }
